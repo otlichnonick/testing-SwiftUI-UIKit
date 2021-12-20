f6dff036c3898d0246dbb2367d73965839f17637
Ivan <37373294+ivansway@users.noreply.github.com>
2021-12-20 16:02:20 +0300

    Add files via upload

---
+++ testing-SwiftUI-UIkit/testing-SwiftUI-UIkit/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  testing-SwiftUI-UIkit
+//
+//  Created by Ivan Myrza on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
